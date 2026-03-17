f69c7532d2e0e677c2de202e4d035bba472648b3
Kashimono <kashimono443@gmail.com>
2026-03-17 19:25:03 +0800

    custom keybind mapping

--- src/main/java/net/aaron/arandommod/KeyHandler.java
+++ src/main/java/net/aaron/arandommod/KeyHandler.java
@@ -2,40 +2,30 @@ package net.aaron.arandommod;
 
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.glfw.GLFW;
 
-@Mod.EventBusSubscriber(modid = ArandomMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+@Mod.EventBusSubscriber(modid = ArandomMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class KeyHandler {
 
-    private static final KeyMapping RANDOM_KEY =
+    public static final KeyMapping RANDOM_KEY =
             new KeyMapping("key.arandommod.randomitem", GLFW.GLFW_KEY_R, "key.categories.misc");
 
-    private static long lastUseTime = 0;
-
     @SubscribeEvent
-    public static void onKeyInput(InputEvent.Key event) {
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
 
         Minecraft mc = Minecraft.getInstance();
 
-        if (RANDOM_KEY.isDown() && mc.player != null) {
-
-            long currentTime = System.currentTimeMillis();
-
-            if (currentTime - lastUseTime < 1) {
-                return;
-            }
-
-            lastUseTime = currentTime;
-
+        if (mc.player != null && RANDOM_KEY.consumeClick()) {
             ModMessages.INSTANCE.sendToServer(new RandomItemPacket());
         }
     }
 
-    @Mod.EventBusSubscriber(modid = ArandomMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    @Mod.EventBusSubscriber(modid = ArandomMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class KeyRegister {
 
         @SubscribeEvent
